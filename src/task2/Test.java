b2f8b784e8d053fd4a2626f4edb24e916b9d086c
MunirTheBest <msunchalyaev@gmail.com>
2023-02-04 13:43:32 +0300

    Fix fix

--- src/task2/Test.java
+++ src/task2/Test.java
@@ -20,12 +20,12 @@ public class Test {
 
         //Найти количество несовершеннолетних (т.е. людей младше 18 лет).
         long countChildren = persons.stream()
-                .filter(x -> x.getAge() <= 18)
+                .filter(x -> x.getAge() < 18)
                 .count();
 
         //Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
         List<String> surnameRecruits = persons.stream()
-                .filter(x -> x.getAge() > 18 && x.getAge() <= 27)
+                .filter(x -> x.getAge() > 18 && x.getAge() < 27)
                 .filter(x -> x.getSex() == Sex.MAN)
                 .map(Person::getName)
                 .collect(Collectors.toList());
@@ -35,14 +35,15 @@ public class Test {
         //(т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
         List<String> surnameWorkersWithHighEducation = persons.stream()
                 .filter(x -> x.getEducation() == Education.HIGHER)
-                .filter(x -> x.getAge()>18 &&
-                        ((x.getSex()==Sex.WOMAN && x.getAge()<=60)
-                        || (x.getSex()==Sex.MAN && x.getAge()<=65)))
+                .filter(x -> x.getAge() >= 18 &&
+                        ((x.getSex() == Sex.WOMAN && x.getAge() < 60)
+                                || (x.getSex() == Sex.MAN && x.getAge() < 65))
+                )
                 .map(Person::getName)
                 .sorted(Comparator.naturalOrder())
                 .collect(Collectors.toList());
 
-        System.out.println("count of children: " + countChildren);
+        System.out.println("Count of children: " + countChildren);
         System.out.println("Recruits: " + surnameRecruits);
         System.out.println("Workers with high education: " + surnameWorkersWithHighEducation);
     }
